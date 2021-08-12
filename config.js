aa4ba69976c38c5ac858c1bc7c560a97a386d2b3
cachecleanerjeet <tkp03463@gmail.com>
2021-08-12 10:05:51 +0000

    Bump to new version

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    BOT_TOKEN: '1352267645:abcdefghifklmnopqrstuvwxyz',
-    API_URL: 'https://7ajk43.deta.dev/location/'
+    BOT_TOKEN: '1352267645:AAEjxI-sAlt82TFxgj-k6dOk7MqBP2Jrj6Q',
+    API_URL: 'https://num1mt.deta.dev/location/'
 }
--- main.js
+++ main.js
@@ -40,9 +40,9 @@ bot.command('help', (ctx) => {
 bot.on('message', async(ctx) => {
     try {
         var weather_data = (await axios.get(`${config.API_URL}${ctx.message.text}`)).data
-        if (weather_data.weather_info.location.city) {
-            var data = weather_data.weather_info
-            ctx.replyWithHTML(`Today's weather in <b><u>${data.location.city}, ${data.location.region}, ${data.location.country}</u></b>\n\n<b>${data.current_observation.condition.text}</b>\n\nCurrent Temperature: <b>${data.current_observation.condition.temperature} °C</b>\nMaximum Temperature: <b>${data.forecasts[0].high} °C</b>\nMinimum Temperature: <b>${data.forecasts[0].low} °C</b>\nHumidity : <b>${data.current_observation.atmosphere.humidity} %</b>\nPressure : <b>${data.current_observation.atmosphere.pressure} mb</b>\nSunrise : <b>${data.current_observation.astronomy.sunrise}</b>\nSunset : <b>${data.current_observation.astronomy.sunset}</b><em>\n\nBrought you by @t_projects</em>`);
+        if (weather_data.error === null && weather_data.result.length !== 0) {
+            var data = weather_data.result[0]
+            ctx.replyWithHTML(`Today's weather in <b><u>${data.location.name}</u></b>\n\n<b>${data.current.skytext}</b>\n\nCurrent Temperature: <b>${data.current.temperature} °C</b>\nFeels like: <b>${data.current.feelslike} °C</b>\nHumidity : <b>${data.current.humidity} %</b><em>\n\nBrought you by @t_projects</em>`);
         } else {
             ctx.reply("Sorry, Nothing Found.\n\nKindly send your city/village name again.\nMake sure if there is a space in your city/village name give that space or try with a big city name nearby you.\n\nIf this problem persists send a message at @t_projects.");
         }
